2b696e5ad8c5cf50aa29e798aa1f2d83fe93ffab
xinyiweizhen <1071060208@qq.com>
2019-08-31 16:08:51 +0800

    初始化项目，并引入tabbar组件

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from "vue"
+import VueRouter from "vue-router";
+
+//路由懒加载组件
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
+
+//1.安装vue-router插件
+Vue.use(VueRouter)
+
+//抽出来的路由映射
+const routes = [
+    {
+        path: '',
+        redirect: '/home'
+      },
+      {
+        path: '/home',
+        component: Home
+      },
+      {
+        path: '/category',
+        component: Category
+      },
+      {
+        path: '/cart',
+        component: Cart
+      },
+      {
+        path: '/profile',
+        component: Profile
+      }
+    ]
+
+//2.创建路由
+const router = new VueRouter({
+    routes,
+    mode:'history',
+});
+
+//3.导出路由
+export default router
