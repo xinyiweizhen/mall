218a102a266245f603542fa49e6195abc2c25a3f
Yaounde <1071060208@qq.com>
2019-09-05 15:09:27 +0800

    项目装了px-to-vw的插件，项目打包上传github

---
+++ dist/js/chunk-2d0c4643.655001a8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c4643"],{"3b42":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h2",[e._v("个人")])},r=[],u={name:"Profile"},a=u,l=t("2877"),o=Object(l["a"])(a,c,r,!1,null,"0f573175",null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0c4643.655001a8.js.map
